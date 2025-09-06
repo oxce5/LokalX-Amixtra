8a9c02485430c577f04e54d4e553ab12a281c40e
oxce5 <avg.gamer@proton.me>
2025-09-06 23:44:32 +0800

    fix: messaging API
    POST to /messages and listen to the websocket (see docs)

--- private/entrypoint.js
+++ private/entrypoint.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const http = require('http');
+
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profile');
 const userContentRoutes = require('./routes/userContent');
 const postsRoutes = require('./routes/posts');
+const messagesRoutes = require('./routes/messages'); 
+const messageWS = require('./services/message-ws');  
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,7 +16,11 @@ app.use('/auth', authRoutes);
 app.use('/profile', profileRoutes);
 app.use('/user-content', userContentRoutes);
 app.use('/posts', postsRoutes);
+app.use('/messages', messagesRoutes); // <-- add this line
 
-app.listen(3000, () => {
+const server = http.createServer(app);
+messageWS.setup(server); 
+server.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
+  console.log('WebSocket server running on ws://localhost:3000');
 });
