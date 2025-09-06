5d48af49aaa5c79a60d1c4ea5a08565e4352937e
oxce5 <avg.gamer@proton.me>
2025-09-06 15:52:35 +0800

    add: user content API

--- private/entrypoint.js
+++ private/entrypoint.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profile');
+const userContentRoutes = require('./routes/userContent');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/auth', authRoutes);
 app.use('/profile', profileRoutes);
+app.use('/user-content', userContentRoutes);
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
