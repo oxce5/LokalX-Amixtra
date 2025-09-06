89773a5263d824649be6825e8b657d3cfe9d1d0e
oxce5 <avg.gamer@proton.me>
2025-09-06 22:49:59 +0800

    add: posts api endpoint

--- private/entrypoint.js
+++ private/entrypoint.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profile');
 const userContentRoutes = require('./routes/userContent');
+const postsRoutes = require('./routes/posts');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 app.use('/profile', profileRoutes);
 app.use('/user-content', userContentRoutes);
+app.use('/posts', postsRoutes);
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
